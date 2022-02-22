074f99f76d6403e6d970f427e2519d3d2eb73342
Roman Tolstosheyev <izobretatel777@mail.ru>
2022-02-23 01:58:40 +0400

    Add Swagger

---
+++ app/src/main/java/com/izobretatel777/chat/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.izobretatel777.chat.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
