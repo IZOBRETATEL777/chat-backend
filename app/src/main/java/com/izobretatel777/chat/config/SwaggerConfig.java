37f00239c0cefa482939202ba38ac3a31e895556
Roman Tolstosheyev <izobretatel777@mail.ru>
2022-02-23 22:30:48 +0400

    Add auotmated database generation

--- app/src/main/java/com/izobretatel777/chat/config/SwaggerConfig.java
+++ app/src/main/java/com/izobretatel777/chat/config/SwaggerConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
---
+++ app/src/main/java/com/izobretatel777/chat/model/User.java
@@ -0,0 +1,20 @@
+package com.izobretatel777.chat.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Table(name="users")
+public class User {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name="login")
+    String login;
+
+    @Column(name="password")
+    String password;
+}
