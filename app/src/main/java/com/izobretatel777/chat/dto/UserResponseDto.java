819dbbe51725ad3b10fa4b638761a96aa34f7f0e
Tolstosheyev Roman <izobretatel777@mail.ru>
2022-03-25 23:03:27 +0400

    Improve response DTO

--- app/src/main/java/com/izobretatel777/chat/dto/UserResponseDto.java
+++ app/src/main/java/com/izobretatel777/chat/dto/UserResponseDto.java
@@ -1,14 +1,20 @@
 package com.izobretatel777.chat.dto;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserResponseDto {
-    private Long id;
-    private String login;
-    private String role;
+    Long id;
+    String login;
+    String name;
+    String surname;
+    String phoneNumber;
+    String role;
 }
