e1c71d5594dc27d7cd37dacfcb8b673271677bd5
Tolstosheyev Roman <izobretatel777@mail.ru>
2022-03-26 21:18:37 +0400

    Improve Swagger home page

--- app/src/main/java/com/izobretatel777/chat/config/SwaggerConfig.java
+++ app/src/main/java/com/izobretatel777/chat/config/SwaggerConfig.java
@@ -1,24 +1,25 @@
 package com.izobretatel777.chat.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import org.springdoc.core.GroupedOpenApi;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
-)
 public class SwaggerConfig {
     @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("public")
-                .pathsToMatch("/**")
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
+                        .title(":Chat! OpenApi reference")
+                        .version("V1.0")
+                        .description("API documentation for :Chat!")
+                        .license(new License().name("MIT").url("http://springdoc.org")));
     }
 }
